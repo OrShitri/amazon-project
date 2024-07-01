988f6c78e98a8b46701529e1a7c70460a1a54041
OrShitri <or.s.jkr@gmail.com>
2024-07-01 09:41:21 +0300

    upt24-06-24

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-
 import './App.css'
+//import Title from './components/shared/Title.jsx'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Container from "react-bootstrap/Container";
+import HomePage from "./pages/HomePage";
+import Footer from './components/shared/Footer';
+import Header from './components/shared/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <div className="d-flex flex-column side-allPage min-width">
+        {/* <ToastContainer position="bottom-center" limit={1} /> */}
+        <Header />
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
