2df1b357b13955b7cbef14e1b73ff482a531d2f9
OrShitri <or.s.jkr@gmail.com>
2024-07-08 16:21:31 +0300

    upt2-01-07-24

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <StoreProvider>
       <HelmetProvider>
         <App />
-        
       </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>,
